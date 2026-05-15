05a1b1fcbfc3f65802a3b19bad242bbad6659b9e
Yoshika Govender <govender_yoshika@bah.com>
2026-05-15 12:07:33 -0400

    fix var name

--- profile.d/server_mode_ps1.sh
+++ profile.d/server_mode_ps1.sh
@@ -7,7 +7,7 @@
 # I recommend using `/etc/environment` to set that var
 
 
-if $prod; then
+if $IS_PROD; then
         PS1='\u\[\e[1m\]@\[\e[34m\]\h\[\e[0;38;5;196m\][PROD]\[\e[0;1m\]:\[\e[32m\]\w\[\e[39m\]\$\[\e[0m\] '
 else
         PS1='\u\[\e[1m\]@\[\e[34m\]\h\[\e[0;38;5;226m\][DEV]\[\e[0;1m\]:\[\e[32m\]\w\[\e[39m\]\$\[\e[0m\] '
